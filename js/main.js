659e94583ddeef5e6837924bdba685dce93cda69
andyreesecups <andyreesecups@yahoo.com>
2017-07-16 20:34:33 -0500

    saveBookmark function added

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// Listen for form submit
+document.getElementById('myForm').addEventListener('submit', saveBookmark);
+
+// Save Bookmark
+function saveBookmark(e) {
+    // Get form values
+    var siteName = document.getElementById('siteName').value;
+    var siteUrl = document.getElementById('siteUrl').value;
+
+    // Prevent form from submitting
+    e.preventDefault();
+}
